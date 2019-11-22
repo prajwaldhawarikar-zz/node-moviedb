a564c5a5d2c61899e6a5d90378f5efc5beb69519
Prajwal Dhawarikar <prajwal.dhawarikar@gmail.com>
2019-11-23 00:46:39 +0530

    config test

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ class ServerConfig {
     }
 
     static getPort() {
-        return process.env.PORT ? process.env.PORT : 3000;
+        return 3000;
     }
 
     static getApiUrl() {
---
+++ tests/config.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-expressions */
+const { describe, it } = require('mocha');
+const chai = require('chai');
+const { serverConfig } = require('../config');
+
+const expect = chai.expect;
+
+describe('CONFIG - should return configs', () => {
+    it('getApiBasePath - should return api basepath', async () => {
+        const result = serverConfig.getApiBasePath();
+        expect(result).to.be.string;
+    });
+    it('getPort - should return server port', async () => {
+        const result = serverConfig.getPort();
+        expect(result).to.be.a('number');
+    });
+    it('getApiUrl - should return api url', async () => {
+        const result = serverConfig.getApiUrl();
+        expect(result).to.be.string;
+    });
+    it('getMovieDbApiKey - should return api basepath', async () => {
+        const result = serverConfig.getMovieDbApiKey();
+        expect(result).to.be.string;
+    });
+});
